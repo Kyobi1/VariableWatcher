bc07a57406da1b5837034d30a4fd537faba70025
Kyobi1 <clement.florant@insa-lyon.fr>
2025-11-24 22:11:53 +0100

    Update examples

--- Example.cpp
+++ Example.cpp
@@ -11,6 +11,30 @@ std::string to_string(const MyStruct& oStruct)
 	return { "m_i : " + std::to_string(oStruct.m_i) + " - m_f : " + std::to_string(oStruct.m_f) };
 }
 
+void Test3()
+{
+	VariableWatcher::Watcher<MyStruct> oTest("oTest");
+
+	MyStruct& oRefTest = oTest;
+	oRefTest.m_i = 12;
+	oRefTest.m_f = 1.6f;
+
+	int* pPtr = pPtr = &oRefTest.m_i;
+	*pPtr = 33;
+}
+
+void Test2()
+{
+	VariableWatcher::Watcher<float> fTest("fTest", 5.3f);
+
+	fTest = 10.2f;
+
+	float* pPtrFloat = &fTest;
+	*pPtrFloat = 4.2f;
+
+	Test3();
+}
+
 void Test()
 {
 	VariableWatcher::Watcher<int> iTest("iTest");
@@ -20,19 +44,11 @@ void Test()
 	int* pPtr = &iTest;
 	*pPtr = 42;
 
-	VariableWatcher::Watcher<float> fTest("fTest", 5.3f);
+	Test2();
+}
 
-	fTest = 10.2f;
-
-	float* pPtrFloat = &fTest;
-	*pPtrFloat = 4.2f;
-
-	VariableWatcher::Watcher<MyStruct> oTest("oTest");
-
-	MyStruct& oRefTest = oTest;
-	oRefTest.m_i = 12;
-	oRefTest.m_f = 1.6f;
-
-	pPtr = &oRefTest.m_i;
-	*pPtr = 33;
+int main()
+{
+	Test();
+	return 0;
 }
